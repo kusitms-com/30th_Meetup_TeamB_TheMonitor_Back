704af5e2e0ae6f98a08e835d8c263c95e0801192
LeeYuJoon <leeyujoon97@gmail.com>
2024-10-10 18:32:37 +0900

    :sparkles: Set User Repository, Service, Entity

---
+++ src/main/java/the_monitor/application/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package the_monitor.application.serviceImpl;
+
+import org.springframework.stereotype.Service;
+import the_monitor.application.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+
+
+}
