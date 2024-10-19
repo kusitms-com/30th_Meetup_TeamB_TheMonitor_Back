bb735ad460208225009aaaf2647c6f972001a0e0
LeeYuJoon <leeyujoon97@gmail.com>
2024-10-20 00:52:31 +0900

    :ambulance: hotfix generateTemporaryPassword
    Related:

--- src/main/java/the_monitor/application/serviceImpl/TemporaryPasswordGenerateServiceImpl.java
+++ src/main/java/the_monitor/application/serviceImpl/TemporaryPasswordGenerateServiceImpl.java
@@ -13,23 +13,25 @@ public class TemporaryPasswordGenerateServiceImpl implements TemporaryPasswordGe
     private static final int PASSWORD_LENGTH = 12;
 
     public String generateTemporaryPassword() {
+
         SecureRandom random = new SecureRandom();
-        StringBuilder password = new StringBuilder(PASSWORD_LENGTH);
+        StringBuilder password = new StringBuilder();
 
         // 특수문자 하나 무작위 선택
         int specialCharIndex = random.nextInt(SPECIAL_CHARACTERS.length());
         char specialChar = SPECIAL_CHARACTERS.charAt(specialCharIndex);
 
+        // 나머지 문자들을 먼저 채워줌
+        for (int i = 0; i < PASSWORD_LENGTH - 1; i++) {
+            int index = random.nextInt(CHARACTERS.length());
+            password.append(CHARACTERS.charAt(index)); // 일반 문자들을 먼저 추가
+        }
+
         // 특수문자 삽입 위치 무작위 선택
         int randomPosition = random.nextInt(PASSWORD_LENGTH);
-        password.insert(randomPosition, specialChar);
-
-        // 나머지 문자 무작위 선택
-        for (int i = 0; i < PASSWORD_LENGTH - 2; i++) {
-            int index = random.nextInt(CHARACTERS.length());
-            password.insert(i < randomPosition ? i : i + 1, CHARACTERS.charAt(index)); // 특수문자 삽입 위치보다 큰 위치에는 1을 더해줌
-        }
+        password.insert(randomPosition, specialChar); // 특수문자 삽입
 
         return password.toString();
     }
+
 }
