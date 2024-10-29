7b65f5e304476c6d31f235cc269d33fa2fd93ed2
LeeYuJoon <leeyujoon97@gmail.com>
2024-10-29 23:42:17 +0900

    :sparkles: Scheduling Enable
    Related:

--- src/main/java/the_monitor/TheMonitorApplication.java
+++ src/main/java/the_monitor/TheMonitorApplication.java
@@ -3,8 +3,11 @@ package the_monitor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 @SpringBootApplication
-@EnableJpaAuditing  // Auditing 기능을 활성화
+@EnableJpaAuditing
+@EnableScheduling
 public class TheMonitorApplication {
 
     public static void main(String[] args) {
