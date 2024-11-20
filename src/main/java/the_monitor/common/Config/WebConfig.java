b01730cd4ec864e51cf32575c0603b7745b26e18
LeeYuJoon <leeyujoon97@gmail.com>
2024-11-20 19:49:15 +0900

    :lock: Domain Cors Config
    Related:

--- src/main/java/the_monitor/common/Config/WebConfig.java
+++ src/main/java/the_monitor/common/Config/WebConfig.java
@@ -21,7 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://the-monitor.o-r.kr", "http://localhost:5173")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
