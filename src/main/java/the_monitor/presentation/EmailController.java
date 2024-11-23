f463ac9d7fbfe447ddc8987aa614821412550123
jihyun <chiiclaro@gmail.com>
2024-11-23 13:54:26 +0900

    Feature: 이메일 전송 기능 구현

--- src/main/java/the_monitor/presentation/EmailController.java
+++ src/main/java/the_monitor/presentation/EmailController.java
@@ -1,15 +1,20 @@
 package the_monitor.presentation;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import the_monitor.application.dto.request.EmailSendRequest;
 import the_monitor.application.dto.request.EmailUpdateRequest;
 import the_monitor.application.dto.response.EmailResponse;
+import the_monitor.application.dto.response.EmailSendResponse;
 import the_monitor.application.service.EmailService;
 import the_monitor.common.ApiResponse;
+
+import java.io.UnsupportedEncodingException;
 
 @RestController
 @RequestMapping("/api/v1/emails")
@@ -33,4 +38,10 @@ public class EmailController {
         return ApiResponse.onSuccessData("이메일 수정 성공", emailService.updateEmails(clientId, emailUpdateRequest, signatureImage));
     }
 
+    @Operation(summary = "이메일 전송", description = "완성된 보고서를 고객사에게 전송합니다.")
+    @PostMapping(value = "/send", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ApiResponse<EmailSendResponse> sendReport(@RequestParam("clientId") Long clinetId,
+                                                     @RequestPart(name="sendEmail") @Valid EmailSendRequest emailSendRequest) throws MessagingException, UnsupportedEncodingException {
+        return ApiResponse.onSuccessData("이메일 전송 성공", emailService.sendReportEmail(clinetId, emailSendRequest.getSubject(), emailSendRequest.getContent()));
+    }
 }
