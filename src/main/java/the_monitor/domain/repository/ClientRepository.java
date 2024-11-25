348f136cd115582197860d947391a68193f5916f
jihyun <chiiclaro@gmail.com>
2024-11-25 14:00:49 +0900

    Fix: 고객사 검색 오류 수정

--- src/main/java/the_monitor/domain/repository/ClientRepository.java
+++ src/main/java/the_monitor/domain/repository/ClientRepository.java
@@ -2,6 +2,7 @@ package the_monitor.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import the_monitor.domain.model.Account;
 import the_monitor.domain.model.Client;
 
 import java.util.List;
@@ -9,5 +10,4 @@ import java.util.List;
 public interface ClientRepository extends JpaRepository<Client, Long> {
     List<Client> findAllByAccountId(@Param("accountId") Long accountId);
 
-    List<Client> findByNameContainingIgnoreCase(String searchText);
-}
+    List<Client> findByAccountAndNameContainingIgnoreCase(Account account, String name);}
