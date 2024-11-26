241de63c8a27db0cee46ede3d56b401d5e51f659
jihyun <chiiclaro@gmail.com>
2024-11-27 03:50:34 +0900

    Response 변경

--- src/main/java/the_monitor/application/dto/response/ReportListResponse.java
+++ src/main/java/the_monitor/application/dto/response/ReportListResponse.java
@@ -1,9 +1,11 @@
 package the_monitor.application.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -12,12 +14,17 @@ public class ReportListResponse {
 
     private Long reportId;
     private String title;
+    private String createdAt;
+    private String updatedAt;
+
 
     @Builder
-    public ReportListResponse(Long reportId, String title) {
+    public ReportListResponse(Long reportId, String title, String createdAt, String updatedAt) {
 
         this.reportId = reportId;
         this.title = title;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
 
     }
 
